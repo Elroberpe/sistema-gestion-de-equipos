7f399aeda4103bafe15348af760f4f6343db5079
CarMorv <carlosmorenovi2000@gmail.com>
2026-06-02 22:44:55 -0500

    feature: Creacion del modulo devolucion(El controllador , el Dao ,) funcionalidades en la vista devoluciones -Buscar prestamo -Mostrar detalle prestamo seleccionado - listar prestamos en la tabla de devoluciones

---
+++ SistemaGestiondeEquipos/src/DTO/PrestamoActivoDTO.java
@@ -0,0 +1,91 @@
+package DTO;
+
+import java.time.LocalDate;
+
+public class PrestamoActivoDTO {
+
+    private int idPrestamo;
+    private int idEquipo;
+    private String codigoEquipo;
+    private String nombreEquipo;
+    private String solicitante;
+    private String dni;
+    private LocalDate fechaPrestamo;
+    private LocalDate fechaDevolucionPrevista;
+    private String estadoPrestamo;
+
+    public PrestamoActivoDTO() {
+    }
+
+    public int getIdPrestamo() {
+        return idPrestamo;
+    }
+
+    public void setIdPrestamo(int idPrestamo) {
+        this.idPrestamo = idPrestamo;
+    }
+
+    public int getIdEquipo() {
+        return idEquipo;
+    }
+
+    public void setIdEquipo(int idEquipo) {
+        this.idEquipo = idEquipo;
+    }
+
+    public String getCodigoEquipo() {
+        return codigoEquipo;
+    }
+
+    public void setCodigoEquipo(String codigoEquipo) {
+        this.codigoEquipo = codigoEquipo;
+    }
+
+    public String getNombreEquipo() {
+        return nombreEquipo;
+    }
+
+    public void setNombreEquipo(String nombreEquipo) {
+        this.nombreEquipo = nombreEquipo;
+    }
+
+    public String getSolicitante() {
+        return solicitante;
+    }
+
+    public void setSolicitante(String solicitante) {
+        this.solicitante = solicitante;
+    }
+    
+    public String getdni() {
+        return dni;
+    }
+
+    public void setDNI(String dni) {
+        this.dni = dni;
+    }
+
+    public LocalDate getFechaPrestamo() {
+        return fechaPrestamo;
+    }
+
+    public void setFechaPrestamo(LocalDate fechaPrestamo) {
+        this.fechaPrestamo = fechaPrestamo;
+    }
+
+    public LocalDate getFechaDevolucionPrevista() {
+        return fechaDevolucionPrevista;
+    }
+
+    public void setFechaDevolucionPrevista(LocalDate fechaDevolucionPrevista) {
+        this.fechaDevolucionPrevista = fechaDevolucionPrevista;
+    }
+
+    public String getEstadoPrestamo() {
+        return estadoPrestamo;
+    }
+
+    public void setEstadoPrestamo(String estadoPrestamo) {
+        this.estadoPrestamo = estadoPrestamo;
+    }
+}
